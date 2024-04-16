714d901ed1e2ec2da651443591105917d5f7b73e
chris2500 <chrisvasquez985@gmail.com>
2024-04-16 13:04:29 -0600

    Interfaz Inicio de sesion

---
+++ src/interfazDeUsuario/NewFXMain.java
@@ -0,0 +1,30 @@
+package interfazDeUsuario;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class NewFXMain extends Application {
+    
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root=FXMLLoader.load(getClass().getResource("/interfazDeUsuario/FXMLInterfazInicioDeSesion.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setScene(scene);  
+        stage.show();    
+                           
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch();
+    }
+    
+}
